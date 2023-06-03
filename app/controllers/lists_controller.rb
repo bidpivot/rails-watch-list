fa580cc963f1e2c1911890987a198fa5bde9d6e5
Lowell Alexander <performance@bidhackers.com>
2023-06-03 14:47:02 -0400

    adjusted db

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,17 +1,35 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = list[list_params]
+    @bookmark = Bookmark.new
+    @list = List.find(params[:id])
   end
 
+  def new
+    @list = List.new
+  end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
